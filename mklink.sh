0df53ad3d36fadabdf55aa56c319028d5103ce4c
ykawa <ykawa@flugels.redirectme.net>
2018-01-12 20:05:34 +0900

    update: relative path

--- mklink.sh
+++ mklink.sh
@@ -1,24 +1,27 @@
 #!/bin/bash -vex
 DATETIME=$(date +%Y%m%d%H%M%S)
 LOCATION=$(cd $(dirname $0); pwd)
+RELATIVE=${LOCATION##$HOME/}
 
 pushd $HOME
-for f in ${LOCATION}/.*
+for f in ${RELATIVE}/*
 do
   [ ! -f $f ] && continue
+
   dotf=$(basename $f)
-  if [ -L $dotf ]; then
-    echo -n " $dotf -> " 1>&2
-    readlink $dotf
+  if [ -L .$dotf ]; then
+    echo -n "update: "
+    readlink .$dotf
+  elif [ -f .$dotf ]; then
+    mv .$dotf $dotf.${DATETIME}
+  elif [ -d .$dotf ]; then
     continue
-  elif [ -f $dotf ]; then
-    mv $dotf $dotf.${DATETIME}
   else
-    echo "WARN: $dotf"
+    echo "no link: $dotf"
     continue
   fi
-  ln -sf $f
-  readlink $dotf
+  ln -sf $f .$dotf
+  readlink .$dotf
 done
 popd
 
