13f737e2e67697b0359a5d6f49c9e79b09e72616
ykawa <ykawa@flugels.redirectme.net>
2019-08-25 23:42:34 +0900

    :tada: add tmux

--- mklink.sh
+++ mklink.sh
@@ -5,22 +5,26 @@ RELATIVE=${LOCATION##$HOME/}
 
 pushd $HOME
 
-touch .bash_profile .bashrc .gitconfig .globalrc .perltidyrc .screenrc .vimrc .vim .pythonstartup .Xmodmap .my.cnf .grcat .dircolors .hyper.js
+touch .bash_profile .bashrc .gitconfig .globalrc .perltidyrc .screenrc .vimrc .vim .pythonstartup .Xmodmap .my.cnf .grcat .dircolors .hyper.js .tmux.conf
 for f in ${RELATIVE}/*
 do
   dotf=$(basename $f)
   if [ -L .$dotf ]; then
-    echo -n "update: "
-    readlink .$dotf
-    mv .$dotf $dotf.${DATETIME}
+    readlink .$dotf | grep -q dotfiles
+    if [ $? == 0 ]; then
+      continue
+    else
+      echo -n "update: .$dotf"
+      mv .$dotf $dotf.${DATETIME}
+    fi
   elif [ -f .$dotf -o -d .$dotf ]; then
+    echo -n "update: .$dotf"
     mv .$dotf $dotf.${DATETIME}
   else
     echo "no link: $dotf"
     continue
   fi
   ln -sf $f .$dotf
-  readlink .$dotf
 done
 popd
 
