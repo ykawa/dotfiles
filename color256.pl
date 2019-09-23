7a3ad35562e707e1caea0a585018b9749b9b9511
ykawa <ykawa@flugels.redirectme.net>
2019-09-23 17:43:17 +0900

    modify

--- color256.pl
+++ color256.pl
@@ -1,10 +1,8 @@
 use strict;
 
-foreach my $i (0..255)
-{
-  printf("\e[48;5;${i}m\e[38;5;15m %03d", $i);
-  printf("\e[33;5;0m\e[38;5;${i}m %03d", $i);
-  printf "\n" if ($i+1) % 16 == 0;
+foreach my $i ( 0 .. 255 ) {
+    printf( "\e[48;5;${i}m\e[38;5;15m %03d", $i );
+    printf( "\e[33;5;0m\e[38;5;${i}m %03d",  $i );
+    printf "\n" if ( $i + 1 ) % 16 == 0;
 }
 print "\033[0m\n";
-
